f38316c315300100219a29a96b961b066deae083
Igor Radaev <cat-malum@yandex.ru>
2022-07-24 20:08:12 +0300

    module 4 task 3 completed

--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,3 +1,28 @@
 function highlight(table) {
-  // ваш код...
+  const tbody = table.childNodes[3]
+  const rows = tbody.children
+
+  for (let i = 0; i < rows.length; i++) {
+    for (let j = 0; j < 4; j++) {
+      let rowsItem = rows[i].children
+
+      if (rowsItem[j].dataset.available === 'true') {
+        rows[i].classList.add('available')
+      } else if (rowsItem[j].dataset.available === 'false') {
+        rows[i].classList.add('unavailable')
+      } else if (!rowsItem[3].getAttribute('data-available')) {
+        rows[i].setAttribute('hidden', 'hidden')
+      }
+
+      if (+rowsItem[j].innerHTML < 18) {
+        rows[i].style.textDecoration = 'line-through'
+      }
+
+      if (rowsItem[j].textContent === 'm') {
+        rows[i].classList.add('male')
+      } else if (rowsItem[j].textContent === 'f') {
+        rows[i].classList.add('female')
+      }     
+    }
+  }
 }
