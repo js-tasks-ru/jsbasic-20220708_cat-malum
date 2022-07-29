7bad5f092a69dc18131aaa16486f2ddaebcdc4e6
Igor Radaev <cat-malum@yandex.ru>
2022-07-29 18:48:05 +0300

    module 5 task 2 completed

--- 5-module/2-task/index.js
+++ 5-module/2-task/index.js
@@ -1,3 +1,9 @@
 function toggleText() {
-  // ваш код...
+  const text = document.querySelector('#text')
+  const button = document.querySelector('.toggle-text-button')
+
+  button.addEventListener('click', () => {
+	  if (!text.getAttribute('hidden')) text.setAttribute('hidden', true)
+	  else if (text.getAttribute('hidden')) text.removeAttribute('hidden', false)
+	})
 }
