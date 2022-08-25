df186a90ed8fa82f0263f7fd1b5fef8d3ba23ca6
Cat-Malum <96351270+Cat-Malum@users.noreply.github.com>
2022-08-25 20:41:21 +0300

    module 8 task 2 completed

--- 8-module/2-task/index.js
+++ 8-module/2-task/index.js
@@ -6,86 +6,49 @@ export default class ProductGrid {
     this.products = products;
     this.filters = {};
     this.render();
-    this.createCards();
-    this.updateFilter(this.filters);
-    // this.removeFilter();
   }
 
-  render() { 
-    this.card = createElement(`
+  render() {
+    this.elem = createElement(`
       <div class="products-grid">
         <div class="products-grid__inner"></div>
       </div>
     `);
+
+    this.renderContent();
   }
 
-  createCards() {
-    this.productsGridInner = this.card.querySelector('.products-grid__inner');
+  renderContent() {
+    this.sub('inner').innerHTML = '';
 
-    for (let k of this.products) {
-      let card = new ProductCard(k);
+    for (let product of this.products) {
+      if (this.filters.noNuts && product.nuts) {
+        continue;
+      }
 
-      this.productsGridInner.appendChild(card.elem);
-    };
-  }
+      if (this.filters.vegeterianOnly && !product.vegeterian) {
+        continue;
+      }
 
-  reRenderCards(filterProducts) {
-    let cards = this.card.querySelectorAll('.card');
-    
-    for (let k of cards) {
-      k.remove();
+      if (this.filters.maxSpiciness !== undefined && product.spiciness > this.filters.maxSpiciness) {
+        continue;
+      }
+
+      if (this.filters.category && product.category != this.filters.category) {
+        continue;
+      }
+
+      let card = new ProductCard(product);
+      this.sub("inner").append(card.elem);
     }
-
-    for (let k of filterProducts) {
-      let card = new ProductCard(k);
-
-      this.productsGridInner.appendChild(card.elem);
-    };
   }
 
   updateFilter(filters) {
-    this.filters = Object.assign(this.filters, filters)
-
-    console.log(this.filters)
-
-    let vegeterian = this.products.filter(fil => {
-      if (fil.vegeterian === this.filters.vegeterianOnly || this.filters.vegeterianOnly === false) {
-        return true;
-      };
-    });
-    let noNuts = this.products.filter(fil => {
-      if (fil.nuts && this.filters.noNuts === true || this.filters.noNuts === false) {
-        return true;
-      };
-    });
-    let category = this.products.filter(fil => {
-      if (fil.category === this.filters.category || this.filters.category === '') {
-        return true;
-      };
-    });
-    let maxSpiciness = this.products.filter(fil => {
-      if (fil.spiciness <= this.filters.maxSpiciness || this.filters.maxSpiciness === false) {
-        return true;
-      };
-    });
-
-    console.log(vegeterian)
-    console.log(noNuts)
-    console.log(category)
-    console.log(maxSpiciness)
-//нужно попробовать отрисовку через цикл, сверить свойства this.filter и предыдущие фильтровки
-    if (filters.hasOwnProperty('vegeterianOnly')) {
-      this.reRenderCards(vegeterian);
-    } else if (filters.hasOwnProperty('noNuts')) {
-      this.reRenderCards(noNuts);
-    } else if (filters.hasOwnProperty('category')) {
-      this.reRenderCards(category);
-    } else if (filters.hasOwnProperty('maxSpiciness')) {
-      this.reRenderCards(maxSpiciness);
-    };
+    Object.assign(this.filters, filters);
+    this.renderContent();
   }
 
-  get elem() {
-    return this.card;
+  sub(ref) {
+    return this.elem.querySelector(`.products-grid__${ref}`);
   }
 }
